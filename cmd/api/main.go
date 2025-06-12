5e878f646a01b1ad3051b5f27607404b3f4339d1
Blue-Davinci <brayancarter67@gmail.com>
2025-06-12 23:34:00 +0300

    Added the server setup: - Added server capability as well as hooks to our application for DI - Added foundations for our routes - MInor helper function addition.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/Blue-Davinci/leadhub-service/internal/database"
 	"github.com/Blue-Davinci/leadhub-service/internal/logger"
 	"github.com/Blue-Davinci/leadhub-service/internal/vcs"
+	_ "github.com/lib/pq"
 	"go.uber.org/zap"
 )
 
@@ -23,6 +28,9 @@ type config struct {
 		maxOpenConns int
 		maxIdleConns int
 		maxIdleTime  string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -48,6 +56,12 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	// CORS configuration
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+
+	})
 	// Parse the flags
 	flag.Parse()
 
@@ -61,4 +75,27 @@ func main() {
 		zap.Int("port", app.config.port),
 		zap.String("env", app.config.env),
 	)
+}
+
+// openDB() opens a new database connection using the provided configuration.
+// It returns a pointer to the sql.DB connection pool and an error value.
+func openDB(cfg config) (*database.Queries, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+	// Use ping to establish new conncetions
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	queries := database.New(db)
+	return queries, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/Blue-Davinci/leadhub-service
 go 1.24.4
 
 require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
