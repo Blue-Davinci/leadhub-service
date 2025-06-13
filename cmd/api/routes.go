b4a328c68aa685223fe2acc15e2d91e8082995f3
Blue-Davinci <brayancarter67@gmail.com>
2025-06-13 11:06:01 +0300

    Ful server setup: - Added server capability including route development - WE are using chi and alice for middleware - Added error helper incase of various errors that may occur - CReated additional helpers and helper function for use in the above update.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
@@ -17,10 +19,42 @@ func (app *application) routes() http.Handler {
 		AllowCredentials: false,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	}))
+	//Use alice to make a global middleware chain.
+	globalMiddleware := alice.New(app.metrics, app.recoverPanic, app.rateLimit, app.authenticate).Then
+
+	// Apply the global middleware to the router
+	router.Use(globalMiddleware)
+
 	// Make our categorized routes
 	v1Router := chi.NewRouter()
+
+	v1Router.Mount("/", app.generalRoutes())
+	v1Router.Mount("/api", app.userRoutes())
 
 	// Moount the v1Router to the main base router
 	router.Mount("/v1", v1Router)
 	return router
+}
+
+// generalRoutes() provides a router for the general routes.
+// Mounted rirectly after our version url. They contaon sanity and
+// health checks. Probably add other AOB's here.
+func (app *application) generalRoutes() chi.Router {
+	generalRoutes := chi.NewRouter()
+	// /debug/vars : for expvar, wrapping it in a handler func for assertion otherwise it complains
+	generalRoutes.Get("/debug/vars", func(w http.ResponseWriter, r *http.Request) {
+		expvar.Handler().ServeHTTP(w, r)
+	})
+	// generalRoutes.Get("/health", app.healthcheckHandler)
+	return generalRoutes
+}
+
+// userRoutes() is a method that returns a chi.Router that contains all the routes for the users
+func (app *application) userRoutes() chi.Router {
+	userRoutes := chi.NewRouter()
+	userRoutes.Post("/", app.registerUserHandler)
+	userRoutes.Post("/authentication", app.createAuthenticationApiKeyHandler)
+	// /activation : for activating accounts
+	userRoutes.Put("/activated", app.activateUserHandler)
+	return userRoutes
 }
